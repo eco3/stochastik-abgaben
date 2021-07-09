f9a8a6a65a1743ef832f4033da482418b38e6b27
Ekrem <ekrem.emre@posteo.de>
2021-07-09 13:18:54 +0200

    added some distribution properties

--- Klausurtools/tools/diskrete_verteilungen.py
+++ Klausurtools/tools/diskrete_verteilungen.py
@@ -1,31 +1,54 @@
 from scipy import stats
 
 
-def bernoulli(p):
-    print(f"stats.bernoulli(p={p})")
+def _calculate_properties(distribution, code_string):
+    print("-----------------------------")
+    print("Erwartungswert\t\t",        f"{code_string}.expect()", "\t",      distribution.expect())
+    print("Varianz\t\t\t",             f"{code_string}.var()", "\t",       distribution.var())
+    print("Standardabweichung\t",      f"{code_string}.std()", "\t",       distribution.std())
+    print()
 
-    return stats.bernoulli(p)
+
+def bernoulli(p):
+    X = stats.bernoulli(p)
+
+    print(f"stats.bernoulli(p={p})", "\t", f"X ~ Ber({p})")
+    _calculate_properties(X, f"stats.bernoulli(p={p})")
+
+    return X
 
 
 def geometrisch(p):
-    print(f"stats.geom(p={p})")
+    X = stats.geom(p)
 
-    return stats.geom(p)
+    print(f"stats.geom(p={p})", "\t", f"X ~ geom({p})")
+    _calculate_properties(X, f"stats.geom(p={p})")
+
+    return X
 
 
 def binomial(n, p):
-    print(f"stats.binom(n={n}, p={p})")
+    X = stats.binom(n, p)
 
-    return stats.binom(n, p)
+    print(f"stats.binom(n={n}, p={p})", "\t", f"X ~ Bin({n}, {p})")
+    _calculate_properties(X, f"stats.binom(n={n}, p={p})")
+
+    return X
 
 
 def poisson(l):
-    print(f"stats.poisson(k={l})")
+    X = stats.poisson(l)
 
-    return stats.poisson(k=l)
+    print(f"stats.poisson({l})", "\t", f"X ~ Po({l})")
+    _calculate_properties(X, f"stats.poisson({l})")
+
+    return X
 
 
-def hyper_geometrisch(N, M, n):
-    print(f"stats.hypergeom(N={N}, M={M}, n={n})")
+def hypergeometrisch(N, M, n):
+    X = stats.hypergeom(N, M, n)
 
-    return stats.hypergeom(N, M, n)
+    print(f"stats.hypergeom(N={N}, M={M}, n={n})", "\t", f"X ~ H({n}, {M}, {N})")
+    _calculate_properties(X, f"stats.hypergeom(N={N}, M={M}, n={n})")
+
+    return X
--- Klausurtools/tools/kontinuierliche_verteilungen.py
+++ Klausurtools/tools/kontinuierliche_verteilungen.py
@@ -1,19 +1,36 @@
 from scipy import stats
 
 
-def gleichverteilung(a, b):
-    print(f"stats.uniform(a={a}, b-a={b-a})")
+def _calculate_properties(distribution, code_string):
+    print("-----------------------------")
+    print("Erwartungswert\t\t",        f"{code_string}.expect()", "\t",      distribution.expect())
+    print("Varianz\t\t\t",             f"{code_string}.var()", "\t",       distribution.var())
+    print("Standardabweichung\t",      f"{code_string}.std()", "\t",       distribution.std())
+    print()
 
-    return stats.uniform(a, b - a)
+
+def gleichverteilung(a, b):
+    X = stats.uniform(a, b - a)
+
+    print(f"stats.uniform(a={a}, b-a={b-a})", "\t", f"X ~ U({a}, {b})")
+    _calculate_properties(X, f"stats.uniform(a={a}, b-a={b-a})")
+
+    return X
 
 
 def exponentialverteilung(l):
-    print(f"stats.expon(scale=1/{l})")
+    X = stats.expon(scale=1 / l)
 
-    return stats.expon(scale=1/l)
+    print(f"stats.expon(scale=1/{l})", "\t", f"X ~ exp({l})")
+    _calculate_properties(X, f"stats.expon(scale=1/{l})")
+
+    return X
 
 
 def normalverteilung(mu, sigma):
-    print(f"stats.norm(mu={mu}, sigma={sigma})")
+    X = stats.norm(mu, sigma)
 
-    return stats.norm(mu, sigma)
+    print(f"stats.norm(mu={mu}, sigma={sigma})", "\t", f"X ~ N({mu}, {sigma})")
+    _calculate_properties(X, f"stats.norm(mu={mu}, sigma={sigma})")
+
+    return X
